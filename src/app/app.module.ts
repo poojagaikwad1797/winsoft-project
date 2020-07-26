99c513fbd2cfd343d3dfb1c5f07d33d3ef7c4fae
Pooja Gaikwad <gaikwadpooja1797@gmail.com>
2020-07-26 09:20:03 +0530

    Created angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Task1Component } from './task1/task1.component';
+import { Task2Component } from './task2/task2.component';
+import { Task3Component } from './task3/task3.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Task1Component,
+    Task2Component,
+    Task3Component,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
